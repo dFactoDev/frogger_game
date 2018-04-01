37849a800bbb56f4d9e6d61cfe516e8e2bbe24dc
brg <brg@VM-Debian8-01.kulu.na>
2018-04-01 21:31:39 +0200

    feat: add enemies

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 // Enemies our player must avoid
+
+// CONSTANTS
+//  Rows for each enemy lane
+var ENEM_ROW1 = 60, 
+        ENEM_ROW2 = 144,
+        ENEM_ROW3 = 230;
+
+//  Speed of enemy X movement
+var MAX_SPEED = 500, MIN_SPEED = 200;
+
+
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -6,6 +18,7 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.speed = Math.random() * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +27,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
+ 
+    if (this.x < ctx.canvas.width) { //if enemy not off canvas
+      this.x += this.speed * dt;
+    } else { // if off canvas
+      // reset speed to new random
+      this.speed = Math.random() * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
+      // reset X pos to start from left again
+      this.x = -100;
+    }
+
+    this.y;
+    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -44,3 +70,14 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var enemy1 = new Enemy(),
+        enemy2 = new Enemy();
+        enemy3 = new Enemy();
+
+enemy1.x = 0, enemy1.y = ENEM_ROW1; 
+enemy2.x = 0, enemy2.y = ENEM_ROW2;
+enemy3.x = 0, enemy3.y = ENEM_ROW3;
+
+var allEnemies = [enemy1,enemy2, enemy3];
+
